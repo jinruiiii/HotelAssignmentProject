e8a002b9c8f723b6c525aff6350fc825f9cc016c
jinruiiii <70333940+jinruiiii@users.noreply.github.com>
2022-04-09 01:45:04 +0800

    Add files via upload

---
+++ HotelAssignmentProject/HotelAssignmentProject 8 April 2022 (3pm)/HotelReservation/src/Assignment/CheckIfInt.java
@@ -0,0 +1,40 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class CheckIfInt {
+
+	
+	public static Boolean checkInt(String s) {
+		
+		try {
+			int num = Integer.parseInt(s);
+			return true;
+		}
+		
+		catch (NumberFormatException e) {
+			return false;
+		}
+		
+	}
+	
+	// s is the user input choice can be adults or children (for printing)
+	public static int getInput(String s,String choice) {
+		
+		Scanner sc = new Scanner(System.in);
+		Boolean end = checkInt(s);
+		String newInput = null;
+		
+		if (end)
+			return Integer.parseInt(s);
+		
+		while(!end) {
+			System.out.println("Invalid input. Enter " + choice + ":");
+			newInput = sc.nextLine();
+			end = checkInt (newInput);
+		}
+		
+		return Integer.parseInt(newInput);
+		
+	}
+}
