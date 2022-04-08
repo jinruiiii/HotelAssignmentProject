b2c28196cf24c4989e00aa08f1243cb8ed51256a
jinruiiii <70333940+jinruiiii@users.noreply.github.com>
2022-04-09 01:48:16 +0800

    Add files via upload

---
+++ Assignment/HotelReservation/src/Assignment/CheckIfDouble.java
@@ -0,0 +1,41 @@
+package Assignment;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class CheckIfDouble {
+
+public static Boolean checkInt(String s) {
+		
+		try {
+			double num = Float.parseFloat(s);
+			return true;
+		}
+		
+		catch (NumberFormatException e) {
+			return false;
+		}
+		
+	}
+	
+	// s is the user input choice can be adults or children (for printing)
+	public static double getInput(String s,String choice) {
+		
+		Scanner sc = new Scanner(System.in);
+		Boolean end = checkInt(s);
+		String newInput = null;
+		
+		if (end)
+			return Float.parseFloat(s);
+		
+		while(!end) {
+			System.out.println("Invalid input. Enter " + choice + ":");
+			newInput = sc.nextLine();
+			end = checkInt (newInput);
+		}
+		
+		return Float.parseFloat(newInput);
+		
+	}
+	
+}
