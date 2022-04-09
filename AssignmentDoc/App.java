be101b1537bd3592ee352fdf6a7c81660752f0ac
Jlorj <75927686+Jlorj@users.noreply.github.com>
2022-04-10 01:37:34 +0800

    Update App.java

--- AssignmentDoc/App.java
+++ AssignmentDoc/App.java
@@ -26,7 +26,8 @@ import java.time.*;
 public class App {
 	
 	/**
-	 * The main program for the Hotel Reservation System.			
+	 * The main program for the Hotel Reservation System.
+	 * @param args				The command line arguments.
 	 * @throws ParseException	if a string fails to be parsed.
 	 * @throws IOException		if the IO operation fails.
 	 */
