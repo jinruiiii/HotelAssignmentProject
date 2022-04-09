546043a51e54a03bbccc52cc470f15203075a324
Jlorj <75927686+Jlorj@users.noreply.github.com>
2022-04-09 17:26:25 +0800

    Create Guest.java

---
+++ AssignmentDoc/Guest.java
@@ -0,0 +1,44 @@
+package Assignment;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @author joell
+ * @author jinrui
+ * @author jamie
+ * @author tan yu
+ *
+ */
+public class Guest {
+	
+
+    private IdentityCard ic;
+    
+    private CreditCard cc = null;
+
+    /**
+     * Constructs a new <Guest> entry that takes in the guest's identification details. 
+     */
+    Guest(){
+        Scanner sc = new Scanner(System.in);
+        this.ic = new IdentityCard();
+    }
+    
+    /**
+     * Retrieves the <code>IdentityCard</code> and its details associated with the <code>Guest<code>, assuming that every <code>Guest<code> has an Identity card.
+     * @return	the <code>Guest<code>'s <code>IdentityCard</code>.
+     */
+    public IdentityCard getIc(){
+        return this.ic;
+    }
+
+    /**
+     * Retrieves the <code>CreditCard</code> and its details associated with the <code>Guest<code>, if the <code>Guest<code> has a credit card.
+     * @return	the <code>Guest<code>'s <code>CreditCard</code>.
+     */
+    public CreditCard getCC(){
+        return cc;
+    }
+
+}
