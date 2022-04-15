e8edb9e7c794174934789a851d6bc43761d789ac
Jlorj <75927686+Jlorj@users.noreply.github.com>
2022-04-15 11:58:04 +0800

    Update Reservation.java

--- AssignmentDoc/Reservation.java
+++ AssignmentDoc/Reservation.java
@@ -14,7 +14,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 /**
- * 
+ * Contains the reservations details of the guest.
  * @author joel
  * @author jinrui
  * @author jamie
